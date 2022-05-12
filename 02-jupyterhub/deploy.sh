7a41b7361be3903f8ca0eeb81cd0b6d88c458ea1
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2022-05-12 14:09:49 -0500

    JHub deployment template added

---
+++ 02-jupyterhub/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Only run once
+# helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+
+# Grab latest updates
+# helm repo update
+
+# kubectl create namespace jupyterhub
+
+# kubectl -n jupyterhub create secret tls jupyterhub-tls --key="tls.key" --cert="tls.crt"
+
+# helm upgrade --install jupyter jupyterhub/jupyterhub \
+helm upgrade --install jupyterhub jupyterhub/jupyterhub \
+	--namespace jupyterhub \
+	--version=1.2.0 \
+	--timeout=60m \
+	--values values.yaml
+
+
